f6dc1990110b0c2d9758c7e83739147799bad148
Joseph-Corcoran <j.f.corcoran@hotmail.com>
2022-04-27 10:44:13 +0100

    update score

--- engineAssign.js
+++ engineAssign.js
@@ -1,41 +1,67 @@
 const engineManufacturer = [
-    "Mercedes",
-    "Red Bull Powertrains",
-    "Renault",
-    "Ferrari"
+  "Mercedes",
+  "Red Bull Powertrains",
+  "Renault",
+  "Ferrari",
 ];
 
-let engineAssign = () =>{
-    let pick = []
-    let text = '';
-    let names = ["Joe", "Sam", "Ben", "Tom"];
+const names = ["Joe", "Sam", "Ben", "Tom"];
 
-    for (let i = pick.length; i <= 3;){
-    let picker = engineManufacturer[Math.floor(Math.random() * engineManufacturer.length)];
-    	
-      if (picker === pick[0] || picker === pick[1] || picker === pick[2] || picker === pick[3]){
-        continue;
-        } else {
-      pick.push(picker)
-          i++
-        }
+let engineAssign = () => {
+  let pick = [];
+  let text = "";
+  let namePick = [];
+
+  for (let i = pick.length; i <= 3; ) {
+    let picker =
+      engineManufacturer[Math.floor(Math.random() * engineManufacturer.length)];
+
+    if (
+      picker === pick[0] ||
+      picker === pick[1] ||
+      picker === pick[2] ||
+      picker === pick[3]
+    ) {
+      continue;
+    } else {
+      pick.push(picker);
+      i++;
     }
-    //loop through pick array to display it
-    for (let i = 0; i < pick.length; i++ ){
-        text += `<li id="engineList">${names[i]} supports: ${pick[i]} powered cars</li>`;
+  }
+  console.log;
+  for (let i = 0; i < names.length; ) {
+    let namePicker = names[Math.floor(Math.random() * names.length)];
+    console.log(namePicker);
+    if (
+      namePicker === namePick[0] ||
+      namePicker === namePick[1] ||
+      namePicker === namePick[2] ||
+      namePicker === namePick[3]
+    ) {
+      continue;
+    } else {
+      namePick.push(namePicker);
+      i++;
     }
-    
-    document.getElementById("engines").innerHTML = `${text}`;
-}
+  }
+
+  //loop through pick array to display it
+  for (let i = 0; i < pick.length; i++) {
+    text += `<li id="engineList">${namePick[i]} supports: ${pick[i]} powered cars</li>`;
+  }
+
+  document.getElementById("engines").innerHTML = `${text}`;
+};
 
 let count = 0;
 
-$('#chooseEngine').on('click', event =>{
-    if(count >= 1){
-        document.getElementById("notAllowed").innerHTML = 'You can only click this once!';
-    } else {
-        count=1;
+$("#chooseEngine").on("click", (event) => {
+  if (count >= 1) {
+    document.getElementById("notAllowed").innerHTML =
+      "You can only click this once!";
+  } else {
+    count = 1;
     engineAssign();
-    }
-  $('#engines').slideDown(500);
-})
+  }
+  $("#engines").slideDown(500);
+});
